d1c71bb4d5cdc4a03e77e0f963e361034038cd25
ggrobelny <ggrobelny@outlook.fr>
2022-01-23 12:40:52 +0100

    horizontal place changement

--- db.py
+++ db.py
@@ -25,7 +25,7 @@ def add_new_user():
         db.commit()
 
 window = Tk()
-window.geometry("450x180")
+window.geometry("650x240")
 #
 error = Message(text="", width=160)
 error.place(x = 30, y = 10)
@@ -33,21 +33,21 @@ error.place(x = 30, y = 10)
 #
 
 label1 = Label(text = "Enter Username:")
-label1.place(x = 30, y = 40)
+label1.place(x = 30, y = 50)
 label1.config(bg = 'lightgreen', padx=0)
 #
 username = Entry(text = "")
-username.place(x=150, y=40, width=200, height=25)
+username.place(x=150, y=50, width=200, height=25)
 #
 label2 = Label(text = "Enter Password:")
-label2.place(x = 30, y = 80)
+label2.place(x = 30, y = 100)
 label2.config(bg = 'lightgreen', padx=0)
 #
 password = Entry(text = "")
-password.place(x=150, y=80, width=200, height=25)
+password.place(x=150, y=100, width=200, height=25)
 # 
 button = Button(text = "Add", command = add_new_user)
-button.place(x=150, y=120, width=75, height=35)
+button.place(x=150, y=160, width=75, height=35)
 
 
 window.mainloop()
