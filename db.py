196cca36b5b553a3867531069acea63dcfc14d8f
ggrobelny <ggrobelny@outlook.fr>
2022-01-23 16:44:00 +0100

    text area + informational.db + button fetch, output withh data at start in terminal

--- db.py
+++ db.py
@@ -1,55 +1,75 @@
 from tkinter import *
-
 import sqlite3
-from unittest import result
+from tkinter import messagebox
 
-with sqlite3.connect("details.db") as db:
+
+with sqlite3.connect("informational.db") as db:
     cursor=db.cursor()
     
-cursor.execute(""" CREATE TABLE IF NOT EXISTS users(id integer PRIMARY KEY AUTOINCREMENT, username text NOT NULL, password text NOT NULL); """)
+cursor.execute(""" CREATE TABLE IF NOT EXISTS baza_d(id integer PRIMARY KEY AUTOINCREMENT, title text NOT NULL, content text NOT NULL); """)
+
+root = Tk()
+root.geometry("1250x700")
+root.title("niezapominajek")
+
+def fetch_data():
+    ciagnij = db.cursor()
+    ciagnij.execute("SELECT * FROM baza_d")
+    dane = ciagnij.fetchall()
+    for dana in dane:
+        print(dana)
+        
+        
 
 
-def add_new_user():
-    newUsername = username.get()
-    newPassword = password.get()
+def add_new_content():
+    newTitle = title.get()
+    newContent = content.get()
 
 
-    cursor.execute("SELECT COUNT(*) from users WHERE username = '"+ newUsername + "' ")
+    cursor.execute("SELECT COUNT(*) from baza_d WHERE title = '"+ newTitle + "' ")
     result = cursor.fetchone()
 
     if int(result[0]) > 0:
-        error["text"] = "Error: Username already exists"
+        error["text"] = "Error: Title already exists"
     else:
         error["text"] = "Added New User"
-        cursor.execute("INSERT INTO users(username, password)VALUES(?,?)",(newUsername, newPassword))
+        cursor.execute("INSERT INTO baza_d(title, content)VALUES(?,?)",(newTitle, newContent))
         db.commit()
 
-window = Tk()
-window.geometry("1250x700")
+
 #
 error = Message(text="", width=160)
 error.place(x = 30, y = 10)
 #
 #
 
-label1 = Label(text = "Enter Username:")
+label1 = Label(text = "Enter Title:")
 label1.place(x = 30, y = 50)
 label1.config(bg = 'lightgreen', padx=0)
 #
-username = Entry(text = "")
-username.place(x=30, y=75, width=200, height=25)
-username.config(bg = 'lightgreen')
+title = Entry(text = "")
+title.place(x=30, y=75, width=200, height=25)
+title.config(bg = 'lightgreen')
 #
-label2 = Label(text = "Enter Password:")
+label2 = Label(text = "Enter Content:")
 label2.place(x = 30, y = 110)
 label2.config(bg = 'lightgreen', padx=0)
 #
-password = Entry(text = "")
-password.place(x=30, y=160, width=200, height=75)
-password.config(bg = 'orange')
+content = Entry(text = "")
+content.place(x=30, y=160, width=200, height=375)
+content.config(bg = 'orange')
 # 
-button = Button(text = "Add", command = add_new_user)
-button.place(x=30, y=250, width=75, height=35)
+button = Button(text = "Add", command = add_new_content)
+button.place(x=30, y=650, width=75, height=35)
+button.config(bg='#a17e41')
+#
+button1 = Button(text = "Fetch", command = fetch_data)
+button1.place(x=110, y=650, width=75, height=35)
+button1.config(bg='#a17e41')
 
+tekst = Text()
+tekst.place(x=300, y=30, width=850, height=570)
 
-window.mainloop()
+fetch_data()
+root.mainloop()
