3a016964b98230ecc63fa9dffcdb77850ac28679
ggrobelny <ggrobelny@outlook.fr>
2022-01-23 13:00:12 +0100

    branch changement + little cosmetics for test

--- db.py
+++ db.py
@@ -18,10 +18,10 @@ def add_new_user():
     result = cursor.fetchone()
 
     if int(result[0]) > 0:
-        error["text"] = "Error: Username already exists"
+        error["text"] = "Error: Title already exists"
     else:
         error["text"] = "Added New User"
-        cursor.execute("INSERT INTO users(username, password)VALUES(?,?)",(newUsername, newPassword))
+        cursor.execute("INSERT INTO users(username, password)VALUES(?,?)",(newUsername))
         db.commit()
 
 window = Tk()
@@ -32,19 +32,19 @@ error.place(x = 30, y = 10)
 #
 #
 
-label1 = Label(text = "Enter Username:")
+label1 = Label(text = "Title:")
 label1.place(x = 30, y = 50)
 label1.config(bg = 'lightgreen', padx=0)
 #
 username = Entry(text = "")
 username.place(x=150, y=50, width=200, height=25)
 #
-label2 = Label(text = "Enter Password:")
+label2 = Label(text = "Content:")
 label2.place(x = 30, y = 100)
 label2.config(bg = 'lightgreen', padx=0)
 #
 password = Entry(text = "")
-password.place(x=150, y=100, width=200, height=25)
+password.place(x=150, y=100, width=200, height=50)
 # 
 button = Button(text = "Add", command = add_new_user)
 button.place(x=150, y=160, width=75, height=35)
